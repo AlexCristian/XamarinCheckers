3328cf43a6eedfb24cf10f6e373e2a4749229c42
snn35 <snn35@drexel.edu>
2019-03-10 19:20:10 -0400

    Added multiple pages for navigation (#3)

---
+++ XamarinCheckers/XamarinCheckers/XamarinCheckers/JoinPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinCheckers
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class JoinPage : ContentPage
+	{
+		public JoinPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
